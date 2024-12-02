0bcf7deab2aa5f5a3be87bd8e801eec794129d18
ABDULQAYYUMRAJPOOT208 <abdulqayyum4144@gmail.com>
2024-12-02 06:45:11 +0500

    Implement the SSG in next-js

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+// pages/index.js
+import { getPostData } from '../utility/GetPostData';
+
+export default function Home({ postData }) {
+    return (
+        <div>
+            <h1>{postData.data.title}</h1>
+            <p>{postData.data.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const postData = getPostData();
+    return {
+        props: {
+            postData
+        }
+    };
+}
